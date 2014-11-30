41b0eb20c2c94aab928d55aaa65283c0590cc5ed
Martin Sandgren <martin@carlmartus.se>
2014-12-01 00:17:09 +0100

    Add: Shader base.

--- src/shader.js
+++ src/shader.js
@@ -1,5 +1,12 @@
+var ES_VERTEX = 1;
+var ES_FRAGMENT = 2;
+
 function esShader(gl) {
 	this.gl = gl;
 	this.program = gl.createProgram();
+}
+
+esShader.prototype.addShaderId = function(shader, idName, type) {
+	this.gl.createShader(type);
 }
 
